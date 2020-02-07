069113bf2228268eea91b162d8535658e68001b3
smokingcuke <w0j8uhv5csio@opayq.net>
2020-02-07 19:36:38 +0100

    Add animated snackbar about privacy policy

---
+++ js/snackbar.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 8000);
+}
