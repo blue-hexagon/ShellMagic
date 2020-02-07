ca39b552181da03740d1224a3bfacbadde74581b
smokingcuke <w0j8uhv5csio@opayq.net>
2020-02-07 22:31:33 +0100

    Rearrange tables + style snackbar containing privacy notice

--- js/snackbar.js
+++ js/snackbar.js
@@ -2,7 +2,7 @@ function snackbar() {
     // Get the snackbar DIV
     let x = document.getElementById("snackbar");
     x.className = "show";// Add the "show" class to DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 8000);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 15000);
 
     function clickHandler(){ // declare a function that updates the state
         x.className = x.className.replace("show", "");
