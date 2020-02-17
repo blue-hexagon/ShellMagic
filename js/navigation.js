cea0f27a4fab3b0417b092c93f32cd0e963c54e5
smokingcuke <w0j8uhv5csio@opayq.net>
2020-02-17 20:49:14 +0100

    Fix responsiveness by autoclosing menu when on handheld devices when navigating side

---
+++ js/navigation.js
@@ -0,0 +1,41 @@
+let windowWidthBig = window.matchMedia("(min-width: 800px)").matches;
+let windowWidthMedium = window.matchMedia("(min-width: 600px), (max-width: 800px )").matches;
+let windowWidthSmall = window.matchMedia("(max-width: 400)").matches;
+
+setNavbarLinkProperties = function clickCloseNavigationBar() {
+    let sideNavigationElement = document.getElementById("sideNavigation");
+    console.log(sideNavigationElement.children.item(0));
+    for (let i = 0; i < sideNavigationElement.childElementCount; i++) {
+        if (window.screen.width < 850) { //If the true SCREEN_WIDTH is less than 600px, i.e. your resolution not your browser resolution?
+            sideNavigationElement.children[i].addEventListener("click", evt => toggleSideNavigation());
+        }
+    }
+};
+
+//document.getElementById("sideNavigation").style.maxHeight = 100%;
+let sideNavigationWidth = function determineDeviceWidth() {
+    if (windowWidthBig) {
+        return '250px';
+    } else if (windowWidthMedium || windowWidthSmall) {
+        return '100%';
+    }
+};
+
+function toggleSideNavigation() {
+    let sideNavigation = document.getElementById("sideNavigation");
+    let main = document.getElementsByTagName("body");
+
+    //sideNavigation.style.height = "100%";
+    //sideNavigation.style.maxHeight = screen.availHeight.toString();
+    if (sideNavigation.style.width >= sideNavigationWidth()) {
+        sideNavigation.style.width = '0';
+        if (windowWidthBig) {
+            main[0].style.marginLeft = "0";
+        }
+    } else {
+        sideNavigation.style.width = sideNavigationWidth();
+        if (windowWidthBig) {
+            main[0].style.marginLeft = sideNavigationWidth();
+        }
+    }
+}
--- js/snackbar.js
+++ js/snackbar.js
@@ -11,3 +11,13 @@ function snackbar() {
     let element = document.getElementById('snackbar'); // grab a reference to your element
     element.addEventListener('click', clickHandler); // associate the function above with the click event
 }
+// <!--<div id="snackbar">We use <u style="text-decoration: dotted; color: #ff327e;">Google Analytics</u> for website analytics
+// and we use <u style="text-decoration: dotted; color: #ff327e;">Freeform.io</u> for mail collection.
+//     <u style="text-decoration: dotted; color: #65ffa0;">We don't collect cookies</u> and we have <u
+// style="text-decoration: dotted; color: #65ffa0;">no intention of selling or distributing <b
+// style="color: darkorange;">any</b> of your data.</u>
+//     <br><i>For more information, please refer to our simple privacy policy in the footer at the bottom of the
+// page.</i>
+// <br><b style="font-family: 'Source Code Pro', serif;">[You may click anywhere on this popup to close it - autocloses
+//     in 15 seconds]</b>
+//     </div>-->
